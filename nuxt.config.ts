937a64af492643dfd9ca33274dc5060463e1ebc7
fido1hn <confidoekeanya@gmail.com>
2024-10-11 01:13:10 +0100

    add primevue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import Aura from '@primevue/themes/aura';
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  modules: ['@primevue/nuxt-module'],
+  devtools: { enabled: true },
+  primevue: {
+    options: {
+      theme: {
+        preset: Aura,
+      },
+    },
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <Button label="Check" icon="pi pi-check" />
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
